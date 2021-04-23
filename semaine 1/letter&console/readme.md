6376bc7821c67ae6ecb999f7852d07f19145a7e3
CamileGhastine <camileghastine@hotmail.com>
2021-04-23 19:50:13 +0000

    readme.md file

---
+++ semaine 1/letter&console/readme.md
@@ -0,0 +1,6 @@
+Pour lancer l'application ouvrir la console et se placer dans src, puis executer la commande :
+
+```php letter_console.php dans le terminal```
+
+Remarque :
+Modifier le nombre de lettre en changeant la valeur de la variable $numberLetters dans le fichier src/letter_console.php
--- semaine 1/letter&console/src/letter_console.php
+++ semaine 1/letter&console/src/letter_console.php
@@ -8,8 +8,11 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 $letter = new Letter;
 $console = new Console();
 
+// Mofifier le nombre de lettres ici.
+$numberLetters = 10;
+
 try {
-    $alphabet = $letter->generate(10);
+    $alphabet = $letter->generate($numberLetters);
     $console->show($alphabet);
 } catch (TypeError $e) {
     echo "Le nombre passer à la methode generate() doit être un entier !";
